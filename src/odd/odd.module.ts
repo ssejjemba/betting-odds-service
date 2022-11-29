e3cf3eab521d556fcd9c8ed37149bb36d8b16858
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-11-29 20:39:40 +0300

    Connect the odd entity with type orm module

--- src/odd/odd.module.ts
+++ src/odd/odd.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Odd } from './entity/odd.entity';
 import { OddController } from './odd.controller';
 import { OddService } from './odd.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Odd])],
   controllers: [OddController],
-  providers: [OddService]
+  providers: [OddService],
 })
 export class OddModule {}
