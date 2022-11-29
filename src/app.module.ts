b477abc0c8e182625a44afe45a434bf0f0271e54
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-11-29 20:46:23 +0300

    Add modul configuration for the postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OddModule } from './odd/odd.module';
 
 @Module({
-  imports: [OddModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'micro_odds',
+      username: 'daniel',
+      password: 'pass',
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true, // never true in production!
+    }),
+    OddModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
